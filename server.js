861f78cffe15328dafb82644a0bd4aa4379e960b
helenmasch <58408606+helenmasch@users.noreply.github.com>
2020-03-26 14:12:36 -0400

    adding handlebars

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const app = express()
 
-const PORT. = process.env.PORT || 8000
+const PORT. = process.env.PORT || 3000
 
 // creates the public folder as route path
 app.use(express.static("public"))
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+// set handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout:
+
+}))
