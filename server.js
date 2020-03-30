fdf5873ffebab072d7e355f908d7ce23d08027ae
helenmasch <58408606+helenmasch@users.noreply.github.com>
2020-03-30 15:36:08 -0400

    changes to server.js

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
-const express = require('express')
-const app = express()
-
-const PORT. = process.env.PORT || 3000
-
-// creates the public folder as route path
-app.use(express.static("public"))
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
-
-// set handlebars
+var express = require('express');
 var exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout:
+var PORT = process.env.PORT || 8080;
 
-}))
+var app = express();
 
-module.exports = 
+// creates the public folder as route path
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: 'main' }));
+app.set("view engine", "handlebars");
+
+var routes = require(".controllers/burger_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
