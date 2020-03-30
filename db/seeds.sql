16d6a3eafe7dbdd73ac26cf6b11e739235f929fe
helenmasch <58408606+helenmasch@users.noreply.github.com>
2020-03-30 19:26:12 -0400

    added more values into the seeds.sql file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,14 @@
 INSERT INTO burgers (burger_name, devoured)
 VALUES ('Cheese Burger', false);
 
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('BBQ Burger', false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Veggie Burger', false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Mushroom Burger', false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Buffalo Burger', false);
