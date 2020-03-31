083c5c11e3b42faed8797af0310ef9f590e1b292
helenmasch <58408606+helenmasch@users.noreply.github.com>
2020-03-31 11:07:04 -0400

    changes made to the server.js file

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,6 +1,29 @@
 var express = require("express");
-var Burger = require("../models/burger.js");
+var burgers = require("../models/burger.js");
 
 var router = express.Router();
+
+router.get("/", function(req, res) {
+    burgers.selectAll(function(data) {
+        var hbsObject = {
+            burgers:data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/", function(req, res){
+    burgers.insertOne(req.body.burger_name), function(){
+        res.redirect("/");
+    };
+});
+
+router.put("/:id", function(req, res) {
+    var condition = req.params,id;
+    burgers.updateOne(condition, function(){
+        res.redirect("/");
+    });
+});
 
 module.exports = router;
