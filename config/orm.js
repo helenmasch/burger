6c42af858d151944ab33f7e4d2ecd50a40ae6d36
helenmasch <58408606+helenmasch@users.noreply.github.com>
2020-03-31 03:45:32 -0400

    select, insert and update functions created

--- config/orm.js
+++ config/orm.js
@@ -10,4 +10,27 @@ var orm = {
             cb(result);
         });
     },
-}
+    insertOne: function(val, cb) {
+        var queryString = `INSERT INTO burgers (burger_name, devoured) VALUES ('${val}', false)`;
+
+        connection.query(queryString, function(err, result){
+            if(err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    // Now updates the devoured boolean for each of the selected burgers
+    updateOne: function(condition, cb) {
+        var queryString = `UPDATE burgers SET devoured = true WHERE id = ${condition}` ;
+
+        console.log(queryString);
+        connection.query(queryString, function(err, result) {
+            if(err) {
+                throw err;
+            }
+            cb(result);
+        });
+    }
+};
+module.exports = orm;
